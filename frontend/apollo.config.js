bd6d77163fc786947391041c64e3de3230383daf
Savanah Trewman <strewman@me.com>
2022-04-26 14:42:37 -0500

    small change

--- frontend/apollo.config.js
+++ frontend/apollo.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    client: {
-      service: {
-        url: "http://localhost:4000/graphql",
-        skipSSLValidation: true,
-      }
+  client: {
+    service: {
+      url: "http://localhost:4000/graphql",
+      skipSSLValidation: true,
     }
   }
+}
